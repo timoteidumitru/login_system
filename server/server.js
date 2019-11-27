522deed49bc7e3d9dbd31152c6c6448578402954
Timotei Dumitru <dumitru.timy@gmail.com>
2019-11-27 15:53:41 +0000

    implement the basic structure of routing system

--- server/server.js
+++ server/server.js
@@ -1,4 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.set("view-engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: "ShokadinuEU" });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs", { info: "This is Login Page!" });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs", { info: "This is Register Page!" });
+});
+
+app.post("/register", (req, res) => {});
+
 app.listen(4000, console.log("Server start listening on port 4000"));
