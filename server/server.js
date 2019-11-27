4a70750cffa13becfa2fb7da6104b27d7ec2fa3e
Timotei Dumitru <dumitru.timy@gmail.com>
2019-11-27 23:33:30 +0000

    implement the very basic version of the login system

--- server/server.js
+++ server/server.js
@@ -1,69 +1,86 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
+const express = require('express')
+const app = express()
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 
-const express = require("express");
-const app = express();
-const bcrypt = require("bcryptjs");
-const passport = require("passport");
-const flash = require("express-flash");
-const session = require("express-session");
-
-const initPassport = require("./passport-config");
-initPassport(
+const initializePassport = require('./passport-config')
+initializePassport(
   passport,
   email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id))
+  id => users.find(user => user.id === id)
+)
 
-const users = [];
+const users = []
 
-app.set("view-engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
-app.use(flash());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { name: req.user.name });
-});
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', { name: req.user.name })
+})
 
-app.get("/login", (req, res) => {
-  res.render("login.ejs");
-});
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs')
+})
 
-app.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true
-  })
-);
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
 
-app.get("/register", (req, res) => {
-  res.render("register.ejs", { info: "This is Register Page!" });
-});
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})
 
-app.post("/register", async (req, res) => {
+app.post('/register', checkNotAuthenticated, async (req, res) => {
   try {
-    const hashedPasw = bcrypt(req.body.password, 10);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
     users.push({
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
-      password: hashedPasw
-    });
-    res.redirect("/login");
+      password: hashedPassword
+    })
+    res.redirect('/login')
   } catch {
-    res.redirect("/register");
+    res.redirect('/register')
   }
-});
+})
 
-app.listen(4000, console.log("Server start listening on port 4000"));
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+app.listen(4000, console.log('Server start to listen on port 4000'))
