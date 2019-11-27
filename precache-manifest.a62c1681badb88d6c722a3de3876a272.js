3a866065128851b523f9495dfb797d516de5d7d6
Timotei Dumitru <dumitru.timy@gmail.com>
2019-11-27 23:47:53 +0000

    Updates

---
+++ precache-manifest.a62c1681badb88d6c722a3de3876a272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165bd4cc9af9c66b44fdd52f6a48e15c",
+    "url": "/login_system/index.html"
+  },
+  {
+    "revision": "0eb6d98a6e6d358d185b",
+    "url": "/login_system/static/css/main.a54e6496.chunk.css"
+  },
+  {
+    "revision": "3d33a3108f877a692265",
+    "url": "/login_system/static/js/2.bc56db55.chunk.js"
+  },
+  {
+    "revision": "0eb6d98a6e6d358d185b",
+    "url": "/login_system/static/js/main.fa7151d0.chunk.js"
+  },
+  {
+    "revision": "dac9d12750963ea8a539",
+    "url": "/login_system/static/js/runtime-main.3fb5de0c.js"
+  }
+]);
